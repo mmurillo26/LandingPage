ac1339f6683c522d28cbdd153a33ff83cad5c34b
Mario Murillo <mmurillo@unitedvirtualities.com>
2020-03-06 14:06:16 -0700

    script functions added, scrolling, modal prototype, focus area

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,1 +1,63 @@
+function comenzar(){
 
+// -------------- Scrollbar
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("mainNav").style.padding = "0.2rem";
+  } else {
+    document.getElementById("mainNav").style.padding = "1rem";
+  }
+}
+// ------------- section focus
+var plans = document.getElementById('plans');
+var plansover = document.getElementById('plansover');
+var about = document.getElementById('about');
+var aboutover = document.getElementById('aboutover');
+var contact = document.getElementById('contact');
+var contactover = document.getElementById('contactover');
+
+
+
+plans.onmouseover = function() {
+  plansover.style.background = "var(--success)";
+  aboutover.style.background = "var(--secondary)";
+  contactover.style.background = "var(--secondary)";
+}
+
+about.onmouseover = function() {
+  plansover.style.background = "var(--secondary)";
+  aboutover.style.background = "var(--success)";
+  contactover.style.background = "var(--secondary)";
+}
+
+contact.onmouseover = function() {
+  plansover.style.background = "var(--secondary)";
+  aboutover.style.background = "var(--secondary)";
+  contactover.style.background = "var(--success)";
+}
+
+// ------------- Modals
+var modal = document.getElementById("myModal");
+
+var opn = document.getElementById("opnModal");
+
+var span = document.getElementById("close");
+
+opn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+}
+window.addEventListener("load",comenzar,false);
