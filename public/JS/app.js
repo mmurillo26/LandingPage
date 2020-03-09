2329c80c7649b2c926a955085ffe116b26c0ade7
Mario Murillo <mmurillo@unitedvirtualities.com>
2020-03-09 01:20:50 -0700

    a better style added

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,60 +1,32 @@
 function page(){
 // -------------- Top Navbar
-var btn = document.getElementById('menuBtn');
+  var btn = document.getElementById('menuBtn');
+  var esc = document.getElementById('nav-list');
 
-btn.onclick = function() {
-  var x = document.getElementById("topNav");
-  if (x.className === "navbar-nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar-nav";
+  btn.onclick = function() {
+    var x = document.getElementById("topNav");
+    if (x.className === "navbar-nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar-nav";
+    }
   }
-}
 
-
-// -------------- Scroll topbar
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("mainNav").style.padding = "0.2rem";
-  } else {
-    document.getElementById("mainNav").style.padding = "1rem";
+  esc.onclick = function() {
+    var y = document.getElementById("topNav");
+      y.className = "navbar-nav";
   }
-}
-// ------------- section focus
-var plans = document.getElementById('plans');
-var plansover = document.getElementById('plansover');
-var about = document.getElementById('about');
-var aboutover = document.getElementById('aboutover');
-var contact = document.getElementById('contact');
-var contactover = document.getElementById('contactover');
-var home = document.getElementById('header');
 
 
-plans.onmouseover = function() {
-  plansover.style.background = "var(--success)";
-  aboutover.style.background = "var(--secondary)";
-  contactover.style.background = "var(--secondary)";
-}
+  // -------------- Scroll topbar
+  window.onscroll = function() {scrollFunction()};
 
-about.onmouseover = function() {
-  plansover.style.background = "var(--secondary)";
-  aboutover.style.background = "var(--success)";
-  contactover.style.background = "var(--secondary)";
-}
-
-contact.onmouseover = function() {
-  plansover.style.background = "var(--secondary)";
-  aboutover.style.background = "var(--secondary)";
-  contactover.style.background = "var(--success)";
-}
-
-home.onmouseover = function() {
-  plansover.style.background = "var(--secondary)";
-  aboutover.style.background = "var(--secondary)";
-  contactover.style.background = "var(--secondary)";
-}
-
+  function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.getElementById("mainNav").style.padding = "0.2rem";
+    } else {
+      document.getElementById("mainNav").style.padding = "1rem";
+    }
+  }
 }
 window.addEventListener("load",page,false);
